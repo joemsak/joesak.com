14fd1f533ef6f4faea6cb25551b9021cb0c0ab78
Joe Sak <joe@joesak.com>
2017-12-21 09:36:54 -0600

    Refactor JS for extension

--- assets/js/photo-feed.js
+++ assets/js/photo-feed.js
@@ -1,85 +1,118 @@
-fetch("https://api.instagram.com/v1/users/self/media/recent/?access_token=***REMOVED***")
-  .then(function(resp) {
-    return resp.json();
-  })
-  .then(function(json) {
-    var data = json.data.slice(0, 12),
-        photoFeed = document.getElementById("photo-feed");
+(function() {
+  var lightbox = {
+    lightboxContainer: document.getElementById("lightbox"),
 
-    data.forEach(function(imgData) {
-      var thumb = document.createElement("a");
+    open: function() {
+      this.lightboxContainer.classList.add("showing");
+    },
 
-      thumb.href = imgData.link;
+    close: function() {
+      this.lightboxContainer.classList.remove("showing");
+    },
 
-      thumb.dataset["fullSizeImg"] = imgData.images
-        .standard_resolution
-        .url;
+    display: function(img, caption) {
+      this._updateHeaderContent(caption);
+      this._updateImgContent(img);
+      this.open();
+    },
 
-      thumb.dataset["caption"] = (imgData.caption || {}).text || "";
-      thumb.dataset["location"] = (imgData.location || {}).name || "";
+    _updateHeaderContent: function(caption) {
+      this.lightboxContainer
+        .querySelector(".lightbox-header div")
+        .replaceWith(caption);
+    },
 
-      thumb.classList.add("mdc-layout-grid__cell", "photo-feed__thumb");
+    _updateImgContent: function(img) {
+      this.lightboxContainer
+        .querySelector(".lightbox-img img")
+        .replaceWith(img);
+    },
+  };
 
-      thumb.style.backgroundImage = "url(" +
-        imgData.images.low_resolution.url +
-      ")";
+  fetch("https://api.instagram.com/v1/users/self/media/recent/?access_token=***REMOVED***")
+    .then(function(resp) { return resp.json(); })
+    .then(function(json) {
+      var data = json.data.slice(0, 12),
+          photoFeed = document.getElementById("photo-feed");
 
-      thumb.innerHTML = "&nbsp;";
+      data.forEach(function(imgData) {
+        var thumb = createThumbnail(imgData);
 
-      thumb.addEventListener('click', function (evt) {
-        evt.preventDefault();
+        photoFeed.append(thumb);
 
-        var captionHTML = "";
-        if (evt.target.dataset.caption != "")
-          captionHTML += evt.target.dataset.caption;
-
-        if (evt.target.dataset.location != "") {
-          if (captionHTML != "")
-            captionHTML += " | ";
-          captionHTML += evt.target.dataset.location;
-        }
-
-        var caption = document.createElement("div");
-
-        if (captionHTML != "") {
-          caption.classList.add("caption");
-          caption.innerHTML = captionHTML;
-        }
-
-        var img = document.createElement("img");
-        img.src = evt.target.dataset.fullSizeImg;
-
-        document.querySelector("#lightbox .lightbox-header div")
-          .replaceWith(caption);
-
-        document.querySelector("#lightbox .lightbox-img img")
-          .replaceWith(img);
-
-        lightbox.open();
+        thumb.addEventListener('click', function (evt) {
+          evt.preventDefault();
+          displayInLightbox(evt.target);
+        });
       });
-
-      photoFeed.append(thumb);
     });
-  });
 
-var lightbox = {
-  lightboxContainer: document.getElementById("lightbox"),
+  function createThumbnail(imgData) {
+    var thumb = document.createElement("a");
 
-  open: function() {
-    this.lightboxContainer.classList.add("showing");
-  },
+    thumb.href = imgData.link;
 
-  close: function() {
-    this.lightboxContainer.classList.remove("showing");
-  },
-};
+    thumb.dataset["fullSizeImg"] = imgData.images
+      .standard_resolution
+      .url;
 
-Array.prototype.forEach.call(
-  document.getElementsByClassName("lightbox-close"),
-  function(el) {
-    el.addEventListener("click", function(e) {
-      e.preventDefault();
-      lightbox.close();
-    });
+    thumb.dataset["caption"] = (imgData.caption || {}).text || "";
+    thumb.dataset["location"] = (imgData.location || {}).name || "";
+
+    thumb.classList.add("mdc-layout-grid__cell", "photo-feed__thumb");
+
+    thumb.style.backgroundImage = "url(" +
+      imgData.images.low_resolution.url +
+    ")";
+
+    thumb.innerHTML = "&nbsp;";
+
+    return thumb;
   }
-);
+
+  function displayInLightbox(imgLink) {
+    var caption = createCaption(imgLink),
+        img = createImage(imgLink);
+
+    lightbox.display(img, caption);
+
+    function createImage(link) {
+      var img = document.createElement("img");
+      img.src = link.dataset.fullSizeImg;
+      return img;
+    }
+
+    function createCaption(link) {
+      var captionHTML = "";
+
+      if (link.dataset.caption != "")
+        captionHTML += link.dataset.caption;
+
+      if (link.dataset.location != "") {
+        if (captionHTML != "")
+          captionHTML += " | ";
+
+        captionHTML += link.dataset.location;
+      }
+
+      var caption = document.createElement("div");
+
+      if (captionHTML != "") {
+        caption.classList.add("caption");
+        caption.innerHTML = captionHTML;
+      }
+
+      return caption;
+    }
+  }
+
+  Array.prototype.forEach.call(
+    document.getElementsByClassName("lightbox-close"),
+    function(el) {
+      el.addEventListener("click", function(e) {
+        e.preventDefault();
+        lightbox.close();
+      });
+    }
+  );
+})();
