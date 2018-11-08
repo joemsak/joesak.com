8e02ab7cb566dbe324f4974859c0308a836191d6
Joe Sak <joe@joesak.com>
2018-11-08 14:05:07 -0600

    Add guages tracking

---
+++ assets/js/guages.js
@@ -0,0 +1,12 @@
+var _gauges = _gauges || [];
+(function() {
+  var t   = document.createElement('script');
+  t.type  = 'text/javascript';
+  t.async = true;
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '5be4969a6c95b62e2cc77747');
+  t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+  t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
+})();
