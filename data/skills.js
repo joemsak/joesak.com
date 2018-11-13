c1e230a453c1d8910ecad46c521b548edbdaa9fd
Joe Sak <joe@joesak.com>
2018-11-13 10:56:03 -0600

    Formatting for visual help

--- data/skills.js
+++ data/skills.js
@@ -4,16 +4,19 @@
   "React",
   "HTML5",
   "CSS3",
+
   "Javascript / ES6+",
   "Sass",
   "Ruby 2.5+",
   "Ruby on Rails 5.2+",
   "PostgreSQL 10+",
+
   "Test-Driven Development",
   "Performance optimization",
   "vim",
   "git",
   "tmux",
+
   "Lean / Agile for humans",
   "Project management",
   "Tech leadership",
