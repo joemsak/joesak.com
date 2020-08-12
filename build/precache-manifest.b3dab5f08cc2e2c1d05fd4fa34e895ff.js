fa65cdb00ea13be10b0ae45b39d94c6efd7f9fa8
Joe Sak <joe@joesak.com>
2020-08-12 16:40:20 -0500

    Build

---
+++ build/precache-manifest.b3dab5f08cc2e2c1d05fd4fa34e895ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a17037f10cfd7b95315a7ea76aba874",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88b69d23aac8f0b373cb",
+    "url": "/static/css/main.0d45f6c0.chunk.css"
+  },
+  {
+    "revision": "bf5e69067dc58241f259",
+    "url": "/static/js/2.050ff710.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.050ff710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88b69d23aac8f0b373cb",
+    "url": "/static/js/main.9de3d199.chunk.js"
+  },
+  {
+    "revision": "2f0626df0166dbe7aaa4",
+    "url": "/static/js/runtime-main.ba31bc1d.js"
+  },
+  {
+    "revision": "43979e73cb055dc44f4ec11c95f9fc63",
+    "url": "/static/media/headshot.43979e73.jpg"
+  }
+]);
