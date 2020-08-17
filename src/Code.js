f470380c52faf8e3c4e3bb42b5a2d75afab5044c
Joe Sak <joe@joesak.com>
2020-08-17 10:45:23 -0500

    Add router with Code, App:

---
+++ src/Code.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function Code() {
+  return (
+    <main>
+      <p>My code samples will go here...</p>
+    </main>
+  )
+}
+
+export default Code;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+
 import App from './App';
+import Code from './Code';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Switch>
+        <Route path="/code"><Code /></Route>
+        <Route path="/"><App /></Route>
+      </Switch>
+    </Router>
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
