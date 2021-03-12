fed7633aa2f49a4df07bff719fbd70e3eb0acb5e
Joe Sak <joe@joesak.com>
2021-03-12 08:05:14 -0600

    Fix logic for page already scrolled at load

--- js/main.js
+++ js/main.js
@@ -108,30 +108,63 @@
   }
 
   const initStickyHeader = () => {
+    const tracker = {
+      previousScroll: window.pageYOffset,
+
+      isScrollingDown() {
+        return window.pageYOffset > this.previousScroll
+      },
+
+      setPreviousScroll() {
+        this.previousScroll = window.pageYOffset
+      },
+    }
+
     const header = document.querySelector('header')
+    const main = document.querySelector('main')
+
     const topBar = document.querySelector('#top-bar')
     const img = document.querySelector('img')
 
-    const IMG_HEIGHT_AT_LOAD = img.getBoundingClientRect().top
+    const originalImgDistanceFromTop = img.getBoundingClientRect().top
+    const originalTopBarHeight = topBar.offsetHeight
 
     const IMG_DIM_SMALL = 50
     const IMG_DIM_LARGE = 200
 
     const ghostElement = document.createElement('div')
-    ghostElement.style.height = `${topBar.offsetHeight}px`
+    ghostElement.style.height = `${originalTopBarHeight}px`
+
+    if (window.pageYOffset > 0 && mainDistanceRatioIsPastLimit())
+      setFixedTopBar()
 
     window.onscroll = e => {
-      if (window.pageYOffset >= IMG_HEIGHT_AT_LOAD) {
-        img.height = IMG_DIM_SMALL
-        img.width = IMG_DIM_SMALL
-        topBar.classList.add('fixedTop')
-        header.appendChild(ghostElement)
+      if (tracker.isScrollingDown() && mainDistanceRatioIsPastLimit()) {
+        setFixedTopBar()
       } else {
-        img.height = IMG_DIM_LARGE
-        img.width = IMG_DIM_LARGE
-        topBar.classList.remove('fixedTop')
-        ghostElement.remove()
+        tracker.setPreviousScroll()
+        setRelativeTopBar()
       }
+    }
+
+    function mainDistanceRatioIsPastLimit() {
+      const mainDistanceFromTop = main.getBoundingClientRect().top
+      const mainDistanceRatio = mainDistanceFromTop / originalTopBarHeight
+      return mainDistanceRatio < 0.75
+    }
+
+    function setFixedTopBar() {
+      img.height = IMG_DIM_SMALL
+      img.width = IMG_DIM_SMALL
+      topBar.classList.add('fixedTop')
+      header.appendChild(ghostElement)
+    }
+
+    function setRelativeTopBar() {
+      img.height = IMG_DIM_LARGE
+      img.width = IMG_DIM_LARGE
+      topBar.classList.remove('fixedTop')
+      ghostElement.remove()
     }
   }
 })()
