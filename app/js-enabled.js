245d35ec69ec3aeb942bd03d8aa7a4ee94747982
Joe Sak <joe@joesak.com>
2021-04-11 10:20:33 -0500

    Start over once again babyyyyyyy

--- app/js-enabled.js
+++
@@ -1,9 +0,0 @@
-export default {
-  init() {
-    const NO_JS = 'no-js'
-    const selector = `.${NO_JS}`
-
-    document.querySelectorAll(selector)
-      .forEach(el => el.classList.remove(NO_JS))
-  }
-}
