2cb4eb63e276758ab5cf7d30d204869e8138a3dd
Joe Sak <joe@joesak.com>
2021-04-14 22:51:08 -0500

    Add the source link

--- src/App.js
+++ src/App.js
@@ -67,6 +67,17 @@ export default function App() {
             </p>
           </article>
         </div>
+
+        <p class="mt-16 text-xs italic text-center">
+          Open the&nbsp;
+          <a
+            className="text-blue-400 hover:text-blue-900"
+            href="https://github.com/joemsak/joemsak.github.io"
+          >
+            source
+          </a>
+          &nbsp;of this website.
+        </p>
       </main>
     </>
   );
