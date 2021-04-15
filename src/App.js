18458d9c8153cc541d951745ab1857d7e56bc094
Joe Sak <joe@joesak.com>
2021-04-14 22:46:00 -0500

    Hey this is good enough for now

--- src/App.js
+++ src/App.js
@@ -6,8 +6,67 @@ export default function App() {
     <>
       <Header />
 
-      <main className="pt-16 text-gray-700">
+      <main className="py-16 text-gray-700">
         <Greeting />
+
+        <div className="w-1/2 m-auto mt-4">
+          <p className="italic text-sm">
+            I'm still rebuilding my new site in React &amp; Tailwind.<br />
+            In the meantime, please enjoy a selection from the Tao te Ching.
+          </p>
+
+          <article className="mt-4">
+            <h1 className="font-bold text-2xl">Soul Food</h1>
+            <h2 className="font-bold text-base">by Lao Tzu</h2>
+            <h3 className="italic text-xs">translation by Ursula Le Guin</h3>
+
+            <p className="mt-4">
+              Everybody on earth knowing<br />
+              that beauty is beautiful<br />
+              makes ugliness.
+            </p>
+
+            <p className="mt-4">
+              Everybody knowing<br />
+              that goodness is good<br />
+              makes wickedness.
+            </p>
+
+            <p className="mt-4">
+              For being and nonbeing<br />
+              arise together;<br />
+              hard and easy<br />
+              complete each other;<br />
+              long and shortshape each other;<br />
+              high and low<br />
+              depend on each other;<br />
+              note and voice<br />
+              make the music together;<br />
+              before and after<br />
+              follow each other.
+            </p>
+
+            <p className="mt-4">
+              That’s why the wise soul<br />
+              does without doing,<br />
+              teaches without talking.
+            </p>
+
+            <p className="mt-4">
+              The things of this world<br />
+              exist, they are;<br />
+              you can’t refuse them.
+            </p>
+
+            <p className="mt-4">
+              To bear and not to own;<br />
+              to act and not lay claim;<br />
+              to do the work and let it go:<br />
+              for just letting it go<br />
+              is what makes it stay.
+            </p>
+          </article>
+        </div>
       </main>
     </>
   );
--- src/Nav.js
+++ src/Nav.js
@@ -4,8 +4,6 @@ export default function Nav() {
   return (
     <nav className="flex">
       <div className="flex">
-        <NavLink>Home</NavLink>
-        <NavLink>Technical Skills</NavLink>
         <NavLink href="/joseph-sak.pdf" target="_blank">Resume</NavLink>
         <NavLink href="//github.com/joemsak">Github</NavLink>
         <NavLink mailTo="joe.sak@hey.com">Email me</NavLink>
